f6918ed6b74ae2eb9281ff39f72cedc897f4b213
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-09-22 17:03:01 +0530

    Modification done of Allow user to enter the details of 10th board. Also user can add details of 12th and graduation if he/she require. Provide a button to add/delete any number of rows for qualification.

--- src/task3.js
+++ src/task3.js
@@ -1,10 +1,16 @@
+//Document Ready Function
 $(document).ready(function () {
-  $("#add").click(function () {
-    var li = $("#input").append(
-      '<li id="item"><input type="text" placeholder="Enetr Class"><input type="text" placeholder="Enetr Board"> <input type="text" placeholder="Enetr Marks">  <input type="text" placeholder="Enetr Division">  <button id="add">+</button>  <button id="del">-</button></li>'
-    );
-  });
-  $("#remove").click(function () {
-    $("#item").remove();
+  $(".container").click((eve) => {
+    //Make li variable that hold the List
+    let li =
+      "<div><input type='text' placeholder='Enter Class'> <input type='text' placeholder='Enter Board'> <input type='text' placeholder='Enter Marks'> <input type='text' placeholder='Enter Division'> <button class='add'>+</button> <button class='del'>-</button></div>";
+    //When class value equal to the event target value then append the value li
+    if (eve.target.className == "add") {
+      $(eve.target.parentNode).after(li);
+    }
+    //Remove list when class value equal to the event target value
+    if (eve.target.className == "del") {
+      $(eve.target.parentElement).remove();
+    }
   });
 });
