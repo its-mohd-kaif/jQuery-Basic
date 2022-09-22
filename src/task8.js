ed5bad612c69daf588bee05e99c450153799caa9
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-09-22 12:58:59 +0530

    Modification of task 8 input blank, pop-up a alert informing user - has been done

--- src/task8.js
+++ src/task8.js
@@ -1,14 +1,35 @@
 $(document).ready(function () {
   $("#submit").click(function () {
-    // var tn=$("#fname").val();
+    //These function do when user start typing in blank field the red color of textbox will be white
+    $("#fname").keyup(function () {
+      if ($("#fname").val() != "") {
+        $("#fname").css("background-color", "white");
+      }
+    });
+    $("#lname").keyup(function () {
+      if ($("#lname").val() != "") {
+        $("#lname").css("background-color", "white");
+      }
+    });
+    $("#fname,#lname").keyup(function () {
+      if ($("#lname").val() != "" && $("#fname").val() != "") {
+        $("#fname").css("background-color", "white");
+        $("#lname").css("background-color", "white");
+      }
+    });
+
+    //These if condition check which field is empty and make foucs on them
     if ($("#fname").val() == "" && $("#lname").val() == "") {
+      $("#fname").focus();
       $("#fname").css("background-color", "Red");
       $("#lname").css("background-color", "Red");
       alert("Blank field not allowed");
     } else if ($("#fname").val() == "") {
+      $("#fname").focus();
       $("#fname").css("background-color", "Red");
       alert("Blank field not allowed");
     } else if ($("#lname").val() == "") {
+      $("#lname").focus();
       $("#lname").css("background-color", "Red");
       alert("Blank field not allowed");
     }
